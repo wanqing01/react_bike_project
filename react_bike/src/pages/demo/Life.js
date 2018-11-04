87e4048a61f2643ed58489f5ea64842040e0a330
wanqing <314115488@qq.com>
2018-11-04 23:14:21 +0800

    初期准备

---
+++ react_bike/src/pages/demo/Life.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Button} from 'antd';
+// import 'antd/dist/antd.css';
+
+export default class extends React.Component {
+    render(){
+        return (
+            <div>
+                test
+                <Button>
+                    ahhahaha
+                </Button>
+            </div>
+        )
+    }
+}
